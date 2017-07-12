b46e18b117bd9f0e8870dc8d28b07f27b152664a
beszti <beszti1995@gmail.com>
2017-07-12 19:37:50 +0200

    alap

---
+++ src/main/java/org/beszti/bmi/App.java
@@ -0,0 +1,13 @@
+package org.beszti.bmi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
